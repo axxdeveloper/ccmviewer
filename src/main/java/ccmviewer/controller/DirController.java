f9d1f460ed16d3a65631fe4310cc82b64406dfc3
user <isaac.liao@ruckuswireless.com>
2018-09-04 14:32:32 +0800

    support folder query

---
+++ src/main/java/ccmviewer/controller/DirController.java
@@ -0,0 +1,66 @@
+package ccmviewer.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+@RestController
+public class DirController {
+
+    private static final String BASE = "/data/ng/ccm/";
+
+    @RequestMapping("/")
+    public String path() {
+        JsonObject json = new JsonObject();
+        JsonArray array = new JsonArray();
+        array.add("config");
+        array.add("knowledge");
+        json.add("paths", array);
+        json.addProperty("isFile", false);
+        return json.toString();
+    }
+
+    @RequestMapping("/**")
+    public String path(HttpServletRequest request) {
+        String parentPath = request.getServletPath();
+        File file = new File(BASE, parentPath);
+        if (file.isDirectory()) {
+            JsonObject json = new JsonObject();
+            JsonArray array = new JsonArray();
+            Arrays.stream(file.list()).forEach(array::add);
+            json.add("paths", array);
+            json.addProperty("isFile", true);
+            return json.toString();
+        } else {
+            JsonObject json = new JsonObject();
+            JsonArray array = new JsonArray();
+            array.add(file.getPath());
+            json.add("paths", array);
+            json.addProperty("isFile", true);
+            json.addProperty("content", toString(file));
+            return json.toString();
+        }
+    }
+
+    private String toString(File file) {
+        try {
+            return IOUtils.toString(new FileReader(file));
+        } catch (IOException e) {
+            return e.toString();
+        }
+    }
+
+
+}
